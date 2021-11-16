906c081d62a475f27826a0442542cbe391495c06
DoniaMahmoud <donia1999mahmoud@gmail.com>
2021-11-16 20:25:22 +0200

    4. Faq Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -3,11 +3,13 @@ import React from "react";
 //Import Page Components
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
 const AboutUs = () => {
   return (
     <>
       <AboutSection />
       <ServicesSection />
+      <FaqSection />
     </>
   );
 };
