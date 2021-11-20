790c57abf7a8d6a8d84a71307aadc788cb2c2797
DoniaMahmoud <donia1999mahmoud@gmail.com>
2021-11-20 18:23:25 +0200

    9. React Router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import react from "react";
-//Import pages (containers)
-import AboutUs from "./pages/AboutUs";
+import React from "react";
+
 //Global Style
 import GlobalStyle from "./components/GlobalStyle";
+//Import pages (containers)
+import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
 import Nav from "./components/Nav";
+//Router
+import { Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Routes>
+        <Route path="/" element={<AboutUs />} />
+        <Route path="/work" element={<OurWork />} />
+        <Route path="/contact" element={<ContactUs />} />
+      </Routes>
     </div>
   );
 }
