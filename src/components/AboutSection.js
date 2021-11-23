6a9c9375b52ceb19a28c4a61bdd897f54a900f74
DoniaMahmoud <donia1999mahmoud@gmail.com>
2021-11-24 00:46:53 +0200

    14. About Animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,6 +3,7 @@ import home1 from "../img/home1.png";
 import { About, Description, Hide, Image } from "../styles";
 //Framer Motion
 import { motion } from "framer-motion";
+import { titleAnimation, fade, photoAnimation } from "../Animation";
 
 const AboutSection = () => {
   return (
@@ -10,25 +11,29 @@ const AboutSection = () => {
       <Description>
         <motion.div>
           <Hide>
-            <motion.h2>We work to make</motion.h2>
+            <motion.h2 variants={titleAnimation}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnimation}>
               your <span>dreams</span> come
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2> true.</motion.h2>
+            <motion.h2 variants={titleAnimation}> true.</motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills.
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt="Guy with a Camera" />
+        <motion.img
+          variants={photoAnimation}
+          src={home1}
+          alt="Guy with a Camera"
+        />
       </Image>
     </About>
   );
